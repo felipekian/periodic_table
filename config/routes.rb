4f98d7d53c4b6aa3ccb63e2087a1a23ad98fcaa1
Felipe Derkian <felipekian@yahoo.com.br>
2021-03-22 23:30:29 +0000

    add router index and show

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
-  get 'table/index'
-
+  #get 'table/index'
+  get 'table', controller:'table', action:'index'
+  get 'table/:name', controller:'table', action:'show'
 
   root to: "welcome#index"
   resources :table, only: %i[index show]
